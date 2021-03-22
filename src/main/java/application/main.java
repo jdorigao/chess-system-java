890f46c3feb99022b2c9e686f75933ebc5a2b968
Juliano Dorigão <jdorigao@gmail.com>
2021-03-22 19:14:41 -0400

    checkmate logic

--- src/main/java/application/main.java
+++ src/main/java/application/main.java
@@ -17,7 +17,7 @@ public class main {
         ChessMatch chessMatch = new ChessMatch();
         List<ChessPiece> captured = new ArrayList<>();
 
-        while (true) {
+        while (!chessMatch.getCheckMate()) {
             try {
                 UI.clearScreen();
                 UI.printMatch(chessMatch, captured);
@@ -46,6 +46,7 @@ public class main {
                 sc.nextLine();
             }
         }
-
+        UI.clearScreen();
+        UI.printMatch(chessMatch, captured);
     }
 }
